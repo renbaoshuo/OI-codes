f291aae913145c9a56a1458d1123f82e02a9435a
Ren Baoshuo <i@baoshuo.ren>
2020-09-20 15:21:40 +0800

    P1928 外星密码
    https://www.luogu.com.cn/record/38657002

---
+++ problem/P1928/P1928.cpp
@@ -0,0 +1,35 @@
+// https://www.luogu.com.cn/record/38657002
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string cnm() {
+    int    n;
+    char   ch;
+    string s   = "";
+    string str = "";
+
+    while (cin >> ch) {
+        if (ch == '[') {
+            cin >> n;
+            str = cnm();
+            while (n--) {
+                s += str;
+            }
+        }
+        else if (ch == ']') {
+            return s;
+        }
+        else {
+            s += ch;
+        }
+    }
+
+    return s;
+}
+
+int main() {
+    cout << cnm() << endl;
+    return 0;
+}
