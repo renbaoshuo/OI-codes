1af14a2a51e0cb97b967d6077b124de645aef1b8
Ren Baoshuo <i@baoshuo.ren>
2020-09-20 17:26:00 +0800

    P1765 手机
    R38667223

---
+++ problem/P1765/P1765.cpp
@@ -0,0 +1,23 @@
+// R38667223
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int num[26] = {1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 4, 1, 2, 3, 1, 2, 3, 4};
+//             a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v. w, x, y, z
+int main() {
+    char c;
+    int  cnt = 0;
+    while (scanf("%c", &c) != EOF) {
+        if (c == ' ') {
+            cnt++;
+        }
+        else if('a' <= c && c <= 'z') {
+            cnt += num[c - 'a'];
+        }
+        // cout << c << ' ' << cnt << endl;
+    }
+    cout << cnt << endl;
+    return 0;
+}
