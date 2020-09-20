69ab2a6ccea0601715cd4baf1e746b798b5548d6
Ren Baoshuo <i@baoshuo.ren>
2020-09-20 17:27:35 +0800

    P1321 单词覆盖还原
    R38664427

---
+++ problem/P1321/P1321.cpp
@@ -0,0 +1,22 @@
+// R38664427
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string s;
+    int    boy, girl;
+    boy = girl = 0;
+    cin >> s;
+
+    for (int i = 0; i < s.size() - 2; i++) {
+        boy += (s[i] == 'b' || s[i + 1] == 'o' || s[i + 2] == 'y');
+    }
+    for (int i = 0; i < s.size() - 3; i++) {
+        girl += (s[i] == 'g' || s[i + 1] == 'i' || s[i + 2] == 'r' || s[i + 3] == 'l');
+    }
+
+    cout << boy << endl << girl << endl;
+    return 0;
+}
