d92a049cea91d69aa326a9f5ec9790f65f751f0b
Ren Baoshuo <i@baoshuo.ren>
2020-09-21 19:49:44 +0800

    P3741 honoka的键盘
    R38667882

---
+++ problem/P3741/P3741.cpp
@@ -0,0 +1,47 @@
+// R38667882
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int count(string str) {
+    int sum = 0;
+    for (int i = 0; i < str.size() - 1; i++)
+        if (str[i] == 'V' && str[i + 1] == 'K')
+            sum++;
+    return sum;
+}
+
+int main() {
+    int    n, max;
+    string str;
+
+    cin >> n >> str;
+
+    max = count(str);
+
+    for (int i = 0; i < str.size(); i++) {
+        if (str[i] == 'V') {
+            str[i] = 'K';
+        }
+        else {
+            str[i] = 'V';
+        }
+
+        int s = count(str);
+
+        if (s > max) {
+            max = s;
+        }
+        if (str[i] == 'V') {
+            str[i] = 'K';
+        }
+        else {
+            str[i] = 'V';
+        }
+    }
+
+    cout << max << endl;
+
+    return 0;
+}
