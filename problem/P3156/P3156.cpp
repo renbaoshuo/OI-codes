d66196fbdc50c2df7d14aff8501d5362e6d6ed60
Ren Baoshuo <i@baoshuo.ren>
2020-09-25 19:38:44 +0800

    P3156 【深基15.例1】询问学号
    R38858817

---
+++ problem/P3156/P3156.cpp
@@ -0,0 +1,18 @@
+// R38858817
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n, m, a[10000005], q;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < m; i++) {
+        cin >> q;
+        cout << a[q-1] << endl;
+    }
+    return 0;
+}
