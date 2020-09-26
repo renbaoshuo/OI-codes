edf01dcfa351c31d2f8d2c535e279b43aff54fad
Ren Baoshuo <i@baoshuo.ren>
2020-09-26 19:38:13 +0800

    P1540 机器翻译
    R38909558

---
+++ problem/P1540/P1540.cpp
@@ -0,0 +1,34 @@
+// R38909558
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int         m, n, t, f = 0, ans = 0;
+    bool        flag = false;
+    vector<int> memory;
+    queue<int>  mq;
+    cin >> m >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> t;
+        ans++;
+        flag = false;
+        for (int j = f; j < memory.size(); j++) {
+            if (memory[j] == t) {
+                ans--;
+                flag = true;
+            }
+        }
+        if (!flag) {
+            memory.push_back(t);
+            mq.push(t);
+        }
+        if (mq.size() > m) {
+            f++;
+            mq.pop();
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
