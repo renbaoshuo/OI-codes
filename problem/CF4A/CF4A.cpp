938465fff64a081ec09460f05d762455c00aec1d
Ren Baoshuo <i@baoshuo.ren>
2020-09-28 20:35:20 +0800

    CF4A Watermelon
    R38988487

---
+++ problem/CF4A/CF4A.cpp
@@ -0,0 +1,12 @@
+// R38988487
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+    cout << (x == 2 ? "NO" : (x % 2 ? "NO" : "YES")) << endl;
+    return 0;
+}
