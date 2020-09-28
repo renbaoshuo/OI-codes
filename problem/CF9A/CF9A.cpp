7b7d7241fed128cfb4dc8577160cce50a1a86279
Ren Baoshuo <i@baoshuo.ren>
2020-09-28 21:07:56 +0800

    CF9A Die Roll
    R38990825

---
+++ problem/CF9A/CF9A.cpp
@@ -0,0 +1,34 @@
+// R38990825
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int    a, b, c;
+    string ans;
+    cin >> a >> b;
+    c = max(a, b);
+    switch (c) {
+        case 1:
+            ans = "1/1";
+            break;
+        case 2:
+            ans = "5/6";
+            break;
+        case 3:
+            ans = "2/3";
+            break;
+        case 4:
+            ans = "1/2";
+            break;
+        case 5:
+            ans = "1/3";
+            break;
+        case 6:
+            ans = "1/6";
+            break;
+    }
+    cout << ans << endl;
+    return 0;
+}
