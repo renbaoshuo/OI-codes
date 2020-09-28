a77c3381833c9fed587f9160e5d9a1299c627043
Ren Baoshuo <i@baoshuo.ren>
2020-09-28 21:42:12 +0800

    P1024 一元三次方程求解
    R38992526

---
+++ problem/P1024/P1024.cpp
@@ -0,0 +1,28 @@
+// R38992526
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+double a, b, c, d;
+int    cnt;
+
+double fc(double x) {
+    return a * x * x * x + b * x * x + c * x + d;
+}
+
+int main() {
+    scanf("%lf%lf%lf%lf", &a, &b, &c, &d);
+    for (double i = -100.00; i <= 100.00; i += 0.001) {
+        double l = i, r = i + 0.001;
+        if (fc(l) * fc(r) < 0) {
+            printf("%.2f ", l);
+            cnt++;
+        }
+        if (cnt == 3) {
+            printf("\n");
+            break;
+        }
+    }
+    return 0;
+}
