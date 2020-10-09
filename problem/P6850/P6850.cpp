5fda59af39197e14006fce948bf049aef50bbddf
Ren Baoshuo <i@baoshuo.ren>
2020-10-09 20:59:36 +0800

    P6850 NOI
    R39590203

---
+++ problem/P6850/P6850.cpp
@@ -0,0 +1,17 @@
+// R39590203
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int a, b, c, d, f, e, g, h, i;
+    cin >> a >> b >> c >> d >> e >> f >> g >> h >> i;
+    if ((h && 50 + a + b + c + d + e + f + g + 5 >= i) || (50 + a + b + c + d + e + f + g >= i)) {
+        cout << "AKIOI" << endl;
+    }
+    else {
+        cout << "AFO" << endl;
+    }
+    return 0;
+}
