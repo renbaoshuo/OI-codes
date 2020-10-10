86b50a0f4fb4e383a2812d1540ec4181931b0810
Baoshuo <i@baoshuo.ren>
2020-10-10 21:35:00 +0800

    765. 字符串加空格
    https://www.acwing.com/problem/content/submission/code_detail/2613527/

---
+++ AcWing/765/765.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    char c;
+    c = getchar();
+    while (c != '\n') {
+        cout << c << ' ';
+        c = getchar();
+    }
+    return 0;
+}
