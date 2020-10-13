d263a9d9826abafe3a05ac32a01359909a0314ff
Ren Baoshuo <i@baoshuo.ren>
2020-10-13 20:52:00 +0800

    P2249 【深基13.例1】查找
    R39757575

---
+++ problem/P2249/P2249.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int         n, t, m;
+    vector<int> nums;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        cin >> t;
+        nums.push_back(t);
+    }
+    for (int i = 0; i < m; i++) {
+        cin >> t;
+        vector<int>::iterator it = lower_bound(nums.begin(), nums.end(), t);
+        if (*it == t) {
+            cout << it - nums.begin() + 1 << ' ';
+        }
+        else {
+            cout << "-1 ";
+        }
+    }
+    return 0;
+}
