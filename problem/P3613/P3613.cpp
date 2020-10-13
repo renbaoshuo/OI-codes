7b818a5bafcf5817287dcb4137274104c9a163a2
Ren Baoshuo <i@baoshuo.ren>
2020-10-13 21:08:38 +0800

    P3613 【深基15.例2】寄包柜
    R39759186

---
+++ problem/P3613/P3613.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    map<int, map<int, int> > m;
+    int n, q, t, i, j, k;
+    cin >> n >> q;
+    while(q--) {
+        cin >> t >> i >> j;
+        if(t == 1) {
+            cin >> k;
+            if(k == 0) {
+                if(m.count(i) && m[i].count(j)) {
+                    m[i].erase(k);
+                    if(m[i].empty()) {
+                        m.erase(i);
+                    }
+                }
+            } else {
+                m[i][j] = k;
+            }
+        } else if(t == 2) {
+            cout << m[i][j] << endl;
+        }
+    }
+    return 0;
+}
