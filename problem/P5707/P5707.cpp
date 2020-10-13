52e887729312de40f8b74e8596e454748d35d086
Ren Baoshuo <i@baoshuo.ren>
2020-10-13 19:00:30 +0800

    P5707 【深基2.例12】上学迟到
    R39747907

---
+++ problem/P5707/P5707.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int s, v, m, h;
+    scanf("%d%d", &s, &v);
+    m = s / v;
+    if(s%v != 0) {
+        m++;
+    }
+    m += 10;
+    h = m / 60;
+    m %= 60;
+    if (h > 7) {
+        printf("%02d", 24 + 7 - h);
+    }
+    else {
+        printf("%02d", 7 - h);
+    }
+    printf(":%02d\n", 60 - m);
+    return 0;
+}
