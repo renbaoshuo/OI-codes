0151225e86e8dd206d7f68dca682e9172fb55259
Ren Baoshuo <i@baoshuo.ren>
2020-10-14 21:07:21 +0800

    P6408 [COCI2008-2009#3] PET
    R39811796

---
+++ problem/P6408/P6408.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int maxi, maxn, t0, t1;
+    for(int i = 0 ; i < 5 ; i++) {
+        t1 = 0;
+        for(int j = 0 ; j < 4 ; j++) {
+            cin >> t0;
+            t1 += t0;
+        }
+        if(t1 > maxn) {
+            maxn = t1;
+            maxi = i+1;
+        }
+    }
+    cout << maxi << ' ' << maxn << endl;
+    return 0;
+}
