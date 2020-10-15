15308f9454521f95980bbd6e38efcb40266c44d4
Ren Baoshuo <i@baoshuo.ren>
2020-10-15 18:12:56 +0800

    P4326 [COCI2006-2007#1] Herman
    R39849330

---
+++ problem/P4326/P4326.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const double pi = 3.14159265358979323;
+
+int main() {
+    int r;
+    scanf("%d", &r);
+    printf("%.6lf\n%.6lf\n", r * r * pi, r * r * 2.0);
+    return 0;
+}
