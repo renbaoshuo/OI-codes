3b2839f7423922de86f5948e38764f107811bedc
Ren Baoshuo <i@baoshuo.ren>
2020-10-15 19:31:46 +0800

    P4327 [COCI2006-2007#1] Okviri
    R39854645

---
+++ problem/P4327/P4327.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string ans[5] = {".", ".", "#", ".", "."};
+    string s;
+    cin >> s;
+    for (int i = 0; i < s.size(); i++) {
+        if ((i + 1) % 3 == 0) {
+            ans[0] += ".*..";
+            ans[1] += "*.*.";
+            ans[2] += (string) "." + s[i] + ".*";
+            ans[3] += "*.*.";
+            ans[4] += ".*..";
+        }
+        else {
+            ans[0] += ".#..";
+            ans[1] += "#.#.";
+            if ((i + 1) % 3 == 2 && i + 1 != s.size()) {
+                ans[2] += (string) "." + s[i] + ".*";
+            }
+            else {
+                ans[2] += (string) "." + s[i] + ".#";
+            }
+            ans[3] += "#.#.";
+            ans[4] += ".#..";
+        }
+    }
+    cout << ans[0] << endl
+         << ans[1] << endl
+         << ans[2] << endl
+         << ans[3] << endl
+         << ans[4] << endl;
+    return 0;
+}
