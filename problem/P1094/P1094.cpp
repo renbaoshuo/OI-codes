06da1a30b29a550d0266e562a233ebc2d50dd5fe
Ren Baoshuo <i@baoshuo.ren>
2020-10-15 21:36:41 +0800

    P1094 纪念品分组
    R39868074

---
+++ problem/P1094/P1094.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int w, n, ans = 0, p[30005];
+    cin >> w >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> p[i];
+    }
+    sort(p, p + n);
+    int l = 0, r = n - 1;
+    while (l <= r) {
+        if (p[l] + p[r] <= w) {
+            l++;
+            r--;
+            ans++;
+        }
+        else {
+            r--;
+            ans++;
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
