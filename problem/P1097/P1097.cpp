616e3b9faf5adec2536f3e8243786be4a945d6ea
Ren Baoshuo <i@baoshuo.ren>
2020-10-15 21:11:36 +0800

    P1097 统计数字
    R39865694

---
+++ problem/P1097/P1097.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    set<int>      s;
+    map<int, int> m;
+    int           n, t;
+    cin >> n;
+    while (n--) {
+        cin >> t;
+        s.insert(t);
+        m[t]++;
+    }
+    for (set<int>::iterator it = s.begin(); it != s.end(); it++) {
+        cout << *it << ' ' << m[*it] << endl;
+    }
+    return 0;
+}
