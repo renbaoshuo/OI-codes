c6e7e720fa1e7ccc768ef47f74c7fb905a5da659
Ren Baoshuo <i@baoshuo.ren>
2020-10-17 20:41:29 +0800

    P1634 禽兽的传染病
    R39988381

---
+++ problem/P1634/P1634.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    unsigned long long x, n, ans=1;
+    cin >> x >> n;
+    for(unsigned long long i = 0 ; i < n ; i++) {
+        ans += ans*x;
+    }
+    cout << ans << endl;
+    return 0;
+}
