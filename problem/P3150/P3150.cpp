0109c1cf92626aaa20b9a950a09fcfe1e7a5a00c
Ren Baoshuo <i@baoshuo.ren>
2020-10-18 21:22:42 +0800

    P3150 pb的游戏(1)
    R40073088

---
+++ problem/P3150/P3150.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n, t;
+    cin >> n;
+    while (n--) {
+        cin >> t;
+        if (t % 2 == 0) {
+            cout << "pb wins" << endl;
+        }
+        else {
+            cout << "zs wins" << endl;
+        }
+    }
+    return 0;
+}
