3ece123c57d831797202fc3e448595a55c7ffa86
Ren Baoshuo <i@baoshuo.ren>
2020-10-22 21:52:13 +0800

    CF25A IQ test
    R40310726

---
+++ problem/CF25A/CF25A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n, a[5], t, o;
+    cin >> n >> a[0] >> a[1] >> a[2];
+    o = (a[0] % 2 + a[1] % 2 + a[2] % 2) / 2;
+    for (int i = 0; i < n; i++) {
+        if (i >= 3) {
+            cin >> t;
+        }
+        else {
+            t = a[i];
+        }
+        if (t % 2 != o) {
+            cout << i+1 << endl;
+            return 0;
+        }
+    }
+    return 0;
+}
