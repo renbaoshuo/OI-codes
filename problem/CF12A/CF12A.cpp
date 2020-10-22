3ef77642a757543ce80f4e1fb5d9737f7465af53
Ren Baoshuo <i@baoshuo.ren>
2020-10-22 21:03:21 +0800

    CF12A Super Agent
    R40304856

---
+++ problem/CF12A/CF12A.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string s, t;
+    cin >> t;
+    s += t;
+    cin >> t;
+    s += t;
+    cin >> t;
+    s += t;
+    for(int i = 0 ; i < 9 ; i++) {
+        if(s[i] != s[8-i]) {
+            cout << "NO" << endl;
+            return 0;
+        }
+    }
+    cout << "YES" << endl;
+    return 0;
+}
