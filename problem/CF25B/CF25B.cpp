f91f4de35817ef71599b7ebde22d4944f28d0617
Ren Baoshuo <i@baoshuo.ren>
2020-10-23 16:22:43 +0800

    CF25B Phone numbers
    R40341264

---
+++ problem/CF25B/CF25B.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int    n;
+    string s;
+    cin >> n >> s;
+    for (int i = 0; i < n; i++) {
+        cout << s[i];
+        if (i % 2 && n % 2 && i < n - 4 || i % 2 && !(n % 2) && i != n - 1) {
+            cout << '-';
+        }
+        if (i == n - 4 && n % 2) {
+            cout << '-' << s[n - 3] << s[n - 2] << s[n - 1];
+            break;
+        }
+    }
+    return 0;
+}
