1dd1d5a132f1f8ce7b36be4991fee28b1d217b06
Ren Baoshuo <i@baoshuo.ren>
2020-10-23 20:29:53 +0800

    CF1146A Love "A"
    R40362120

---
+++ problem/CF1146A/CF1146A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    char c;
+    int  n = 0, a = 0;
+    while (cin >> c) {
+        n++;
+        if (c == 'a') {
+            a++;
+        }
+    }
+    cout << min(a * 2 - 1, n) << endl;
+    return 0;
+}
