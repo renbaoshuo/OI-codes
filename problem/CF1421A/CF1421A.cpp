07eb0d6a2875c58ab0f9531c5da255db43737f06
Ren Baoshuo <i@baoshuo.ren>
2020-10-23 20:15:32 +0800

    CF1421A XORwice
    R40360103

---
+++ problem/CF1421A/CF1421A.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t, a, b;
+    cin >> t;
+    while(t--) {
+        cin >> a >> b;
+        cout << a + b - (a & b) * 2 << endl;
+    }
+    return 0;
+}
