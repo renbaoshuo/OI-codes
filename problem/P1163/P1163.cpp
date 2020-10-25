9575e7d6415714d5787473d1b50acaf85eea9056
Ren Baoshuo <i@baoshuo.ren>
2020-10-25 20:45:23 +0800

    P1163 银行贷款
    R40521435

---
+++ problem/P1163/P1163.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    double n, m, k, l, r, mid;
+    cin >> n >> m >> k;
+    l = 00.0;
+    r = 10.0;
+    while (r - l >= 0.0001) {
+        mid = (l + r) / 2;
+        if (pow(1.0 / (1.0 + mid), k) >= 1 - n / m * mid) {
+            r = mid;
+        }
+        else {
+            l = mid;
+        }
+    }
+    cout << fixed << setprecision(1) << l * 100 << endl;
+    return 0;
+}
