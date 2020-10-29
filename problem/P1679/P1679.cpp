d228cdaab0102332c3e2ba182d43ecbbe63e292d
Ren Baoshuo <i@baoshuo.ren>
2020-10-29 21:57:30 +0800

    P1679 神奇的四次方数
    R40788402

---
+++ problem/P1679/P1679.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, ans = 999999;
+
+void dfs(int cnt, int k, int last) {
+    if (k > ans) {
+        return;
+    }
+    if (cnt > n) {
+        return;
+    }
+    if (cnt == n) {
+        ans = min(ans, k);
+        return;
+    }
+    int i = last;
+    while (i*i*i*i <= n - cnt) {
+        i++;
+    }
+    while (i >= last) {
+        dfs(cnt + i * i * i * i, k + 1, i);
+        i--;
+    }
+    return;
+}
+
+int main() {
+    cin >> n;
+    dfs(0, 0, 1);
+    cout << ans << endl;
+    return 0;
+}
