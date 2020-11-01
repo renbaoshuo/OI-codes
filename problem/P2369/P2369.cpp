ed5da1a9ce00953c7a42a97a1bd838a210e3fd64
Ren Baoshuo <i@baoshuo.ren>
2020-11-01 20:36:16 +0800

    P2369 EXCEEDED WARNING A
    R41037866

---
+++ problem/P2369/P2369.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    short a[1000005];
+    int   n, m;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    sort(a, a + n);
+    for (int i = 0; i < m; i++) {
+        cout << a[i] << endl;
+    }
+}
