e4204781cdf8d3184af476843d72f8c857447ea0
Ren Baoshuo <i@baoshuo.ren>
2020-11-02 17:05:29 +0800

    P3378 【模板】堆
    R41079850

---
+++ problem/P3378/P3378.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    priority_queue<int, vector<int>, greater<int> > q;
+    int n;
+    cin >> n;
+    while(n--) {
+        int op;
+        cin >> op;
+        if(op == 1) {
+            int x;
+            cin >> x;
+            q.push(x);
+        }
+        else if(op == 2) {
+            cout << q.top() << endl;
+        }
+        else if(op == 3) {
+            q.pop();
+        }
+    }
+    return 0;
+}
