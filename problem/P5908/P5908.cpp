581ece61b9a5095e30203c316c9d23624373dccf
Ren Baoshuo <i@baoshuo.ren>
2020-11-05 08:12:33 +0800

    P5908 猫猫和企鹅
    R41275298

---
+++ problem/P5908/P5908.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int         n, a, b, d, ans;
+vector<int> g[100005];
+bool        vis[100005];
+
+void dfs(int now, int dis) {
+    vis[now] = true;
+    if (dis == d) {
+        return;
+    }
+    for (int i = 0; i < g[now].size(); i++) {
+        if (!vis[g[now][i]]) {
+            ans++;
+            dfs(g[now][i], dis + 1);
+        }
+    }
+    return;
+}
+
+int main() {
+    memset(vis, 0x00, sizeof(vis));
+    cin >> n >> d;
+    for (int i = 1; i < n; i++) {
+        cin >> a >> b;
+        g[a].push_back(b);
+        g[b].push_back(a);
+    }
+    dfs(1, 0);
+    cout << ans << endl;
+    return 0;
+}
