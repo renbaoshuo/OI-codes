fefd3078e643b09340067524a51b10c1d9ac87df
Ren Baoshuo <i@baoshuo.ren>
2020-11-05 10:44:51 +0800

    P3916 图的遍历
    R41283496

---
+++ problem/P3916/P3916.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int         n, m, u, v, a[100010];
+vector<int> g[100010];
+
+void dfs(int x, int d) {
+    if (a[x]) {
+        return;
+    }
+    a[x] = d;
+    for (int i = 0; i < g[x].size(); i++) {
+        dfs(g[x][i], d);
+    }
+}
+
+int main() {
+    cin >> n >> m;
+    for (int i = 0; i < m; i++) {
+        cin >> u >> v;
+        g[v].push_back(u);
+    }
+    for (int i = n; i > 0; i--) {
+        dfs(i, i);
+    }
+    for (int i = 1; i <= n; i++) {
+        cout << a[i] << ' ';
+    }
+    cout << endl;
+    return 0;
+}
