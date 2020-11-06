4477dab2305a3e628b457028a87de1a61e25f0a7
Ren Baoshuo <i@baoshuo.ren>
2020-11-06 09:36:49 +0800

    P1664 每日打卡心情好
    R41363285

---
+++ problem/P1664/P1664.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n, d = 0, ans = 0, m = 0, now;
+    cin >> n;
+    while (n--) {
+        cin >> now;
+        if (now) {
+            if (m > 0) {
+                d -= pow(2, m - 1);
+            }
+            if (d < 0) {
+                d = 0;
+            }
+            ans++;
+            d++;
+            m = 0;
+            if (d >= 3) {
+                ans++;
+            }
+            if (d >= 7) {
+                ans++;
+            }
+            if (d >= 30) {
+                ans++;
+            }
+            if (d >= 120) {
+                ans++;
+            }
+            if (d >= 365) {
+                ans++;
+            }
+        }
+        else {
+            m++;
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
