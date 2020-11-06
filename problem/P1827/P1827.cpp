5227876e8da1d21e6411d53a5a9c2578a73f0b55
Ren Baoshuo <i@baoshuo.ren>
2020-11-06 16:50:24 +0800

    P1827 [USACO3.4]美国血统 American Heritage
    R41391823

---
+++ problem/P1827/P1827.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string pre, in, post;
+
+void dfs(string pre, string in) {
+    if (pre.empty()) {
+        return;
+    }
+    char root = pre[0];
+    int  k    = in.find(root);
+    pre.erase(pre.begin());
+    string leftpre  = pre.substr(0, k);
+    string rightpre = pre.substr(k);
+    string leftin   = in.substr(0, k);
+    string rightin  = in.substr(k + 1);
+    dfs(leftpre, leftin);
+    dfs(rightpre, rightin);
+    post.push_back(root);
+}
+
+int main() {
+    cin >> in >> pre;
+    dfs(pre, in);
+    cout << post << endl;
+    return 0;
+}
