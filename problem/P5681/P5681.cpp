ce30a7194ae53a6c29299dc011baf8f8bd7ada43
Ren Baoshuo <i@baoshuo.ren>
2020-11-06 22:09:57 +0800

    P5681 [CSPJX2019]面积【民间数据】
    R41434567

---
+++ problem/P5681/P5681.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    long long a, b, c;
+    cin >> a >> b >> c;
+    cout << (a * a > b * c ? "Alice" : "Bob") << endl;
+    return 0;
+}
