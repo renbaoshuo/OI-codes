d25daeb46690f3df713bddee993319125609a0d0
Ren Baoshuo <i@baoshuo.ren>
2020-11-07 23:47:19 +0800

    P1168 中位数
    R41500325

--- problem/P1168/P1168.cpp
+++ problem/P1168/P1168.cpp
@@ -1,16 +1,31 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 
 using namespace std;
 
 int main() {
-    int         n, t;
-    vector<int> a;
+    int n, a[100005], k;
+    priority_queue<int, vector<int>, less<int> > s;
+    priority_queue<int, vector<int>, greater<int> > l;
     cin >> n;
-    for (int i = 1; i <= n; i++) {
-        cin >> t;
-        a.insert(upper_bound(a.begin(), a.end(), t), t);
-        if (i % 2) {
-            cout << a[(i - 1) / 2] << endl;
+    for(int i = 1 ; i <= n ; i++) {
+        cin >> a[i];
+        k = i/2+1;
+        if(!l.empty() && a[i] >= l.top()) {
+            s.push(a[i]);
+        }
+        else {
+            l.push(a[i]);
+        }
+        while(l.size() < k) {
+            l.push(s.top());
+            s.pop();
+        }
+        while(s.size() < k) {
+            s.push(l.top());
+            l.pop();
+        }
+        if(i%2) {
+            cout << s.top() << endl;
         }
     }
     return 0;
