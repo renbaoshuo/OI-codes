fff8697e472cae2c3b677a864807ef7375d683af
Ren Baoshuo <i@baoshuo.ren>
2020-11-13 18:29:30 +0800

    P7071 优秀的拆分
    R41824043

---
+++ problem/P7071/P7071.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    if (n & 1) {
+        cout << -1 << endl;
+    }
+    else {
+        for (int i = 24; i > 0; i--) {
+            if (n >> i & 1) {
+                cout << (1 << i) << ' ';
+            }
+        }
+    }
+    return 0;
+}
