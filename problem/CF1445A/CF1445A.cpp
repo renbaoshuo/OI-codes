26c7669dc67ac67950922a2860b8904905fa7e78
Ren Baoshuo <i@baoshuo.ren>
2020-11-14 22:15:30 +0800

    CF1445A Array Rearrangment
    R41908028

---
+++ problem/CF1445A/CF1445A.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int  n, x, a[55], b[55];
+        bool flag = false;
+        cin >> n >> x;
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        for (int i = 0; i < n; i++) {
+            cin >> b[i];
+        }
+        sort(a, a + n);
+        sort(b, b + n);
+        reverse(b, b + n);
+        for (int i = 0; i < n; i++) {
+            if (a[i] + b[i] > x) {
+                flag = true;
+            }
+        }
+        if (flag) {
+            cout << "No" << endl;
+        }
+        else {
+            cout << "Yes" << endl;
+        }
+    }
+    return 0;
+}
+
