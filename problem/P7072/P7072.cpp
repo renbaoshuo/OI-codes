453b1808b732805982a3a4d7f5b1595f0cfa59e4
Ren Baoshuo <i@baoshuo.ren>
2020-11-14 22:14:22 +0800

    P7072 直播获奖
    R41907572

---
+++ problem/P7072/P7072.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int         n, w, t;
+    vector<int> score;
+    cin >> n >> w;
+    for (int i = 1; i <= n; i++) {
+        cin >> t;
+        score.insert(lower_bound(score.begin(), score.end(), t), t);
+        cout << score[score.size() - max(1, i * w / 100)] << ' ';
+    }
+    return 0;
+}
