f493824604839ca711f337c98cd11ce6dc8a1b6f
Ren Baoshuo <i@baoshuo.ren>
2020-11-15 16:09:16 +0800

    P1449 后缀表达式
    R41950224

---
+++ problem/P1449/P1449.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    stack<int> st;
+    char       ch;
+    int        a, b, t = 0;
+    while (cin >> ch, ch != '@') {
+        if (ch == '.') {
+            st.push(t);
+            t = 0;
+        }
+        else if('0' <= ch && ch <= '9') {
+            t *= 10;
+            t += ch - '0';
+        }
+        else {
+            b = st.top(); st.pop();
+            a = st.top(); st.pop();
+            if (ch == '+') {
+                st.push(a + b);
+            }
+            else if (ch == '-') {
+                st.push(a - b);
+            }
+            else if (ch == '*') {
+                st.push(a * b);
+            }
+            else if (ch == '/') {
+                st.push(a / b);
+            }
+        }
+    }
+    cout << st.top() << endl;
+    return 0;
+}
