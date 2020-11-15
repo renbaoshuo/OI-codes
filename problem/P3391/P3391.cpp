61a5da0f2018710385edc96828dc61ef2b8d7ab3
Ren Baoshuo <i@baoshuo.ren>
2020-11-15 17:56:31 +0800

    P3391 【模板】文艺平衡树 (80' Code)
    R41959184

---
+++ problem/P3391/P3391.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    vector<int> a;
+    int         n, m, l, r;
+    scanf("%d%d", &n, &m);
+    for (int i = 0; i <= n; i++) {
+        a.push_back(i);
+    }
+    for (int i = 0; i < m; i++) {
+        scanf("%d%d", &l, &r);
+        reverse(a.begin() + l, a.begin() + r + 1);
+    }
+    for (int i = 1; i < a.size(); i++) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+    return 0;
+}
