7906f5ef3a640515b8a796459b8236b1ee926856
Ren Baoshuo <i@baoshuo.ren>
2020-11-16 18:42:08 +0800

    P3871 [TJOI2010]中位数
    R42008699

---
+++ problem/P3871/P3871.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n,m, t;
+    string op;
+    vector<int> nums;
+    cin >> n;
+    for(int i = 0 ; i < n ; i++) {
+        cin >> t;
+        nums.insert(lower_bound(nums.begin(), nums.end(), t), t);
+    }
+    cin >> m;
+    for(int i = 0 ; i < m ; i++) {
+        cin >> op;
+        if(op == "add") {
+            cin >> t;
+            nums.insert(lower_bound(nums.begin(), nums.end(), t), t);
+        }
+        else {
+            cout << nums[nums.size() / 2 - (nums.size() & 1 ^ 1)] << endl;
+        }
+    }
+    return 0;
+}
