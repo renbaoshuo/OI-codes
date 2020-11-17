9f4f85ea725d5247a1238a4e57de614d89153fbd
Ren Baoshuo <i@baoshuo.ren>
2020-11-17 19:27:36 +0800

    P1897 电梯里的爱情
    R42070546

---
+++ problem/P1897/P1897.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int        n, a[100005];
+    queue<int> q;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    sort(a, a + n);
+    int h, s, t;
+    h = a[n - 1];
+    s = unique(a, a + n) - a;
+    t = h * (4 + 6) + n;
+    for (int i = 0; i < s; i++) {
+        if (a[i]) {
+            t += 5;
+        }
+    }
+    cout << t << endl;
+    return 0;
+}
