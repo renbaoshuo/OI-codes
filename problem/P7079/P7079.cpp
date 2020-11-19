7d168483040f7da33b14245f17403733c5ac896c
Ren Baoshuo <i@baoshuo.ren>
2020-11-19 17:27:53 +0800

    P7079 [NWRRC2013]Arrangement of Contest
    R42171430

---
+++ problem/P7079/P7079.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int    n, f[105], ans = 0;
+    string s[105];
+    char   now = 'A';
+    memset(f, 0x00, sizeof(f));
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> s[i];
+    }
+    sort(s, s + n);
+    for (int i = 0; i < n; i++) {
+        if (s[i][0] == now) {
+            ans++;
+            now++;
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
