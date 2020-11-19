3d98210ff3a90af9b73444148cdaeab8e74ba22e
Ren Baoshuo <i@baoshuo.ren>
2020-11-19 19:39:25 +0800

    P1605 迷宫
    R42180362

---
+++ problem/P1605/P1605.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, m, t, sx, sy, fx, fy, ans;
+int vis[10][10];
+
+void dfs(int x, int y) {
+    if (x < 1 || y < 1 || x > n || y > n || vis[x][y] == -1 || vis[x][y] == 1) {
+        return;
+    }
+    if (x == fx && y == fy) {
+        ans++;
+        return;
+    }
+    vis[x][y] = 1;
+    dfs(x - 1, y);
+    dfs(x, y - 1);
+    dfs(x, y + 1);
+    dfs(x + 1, y);
+    vis[x][y] = 0;
+}
+
+int main() {
+    cin >> n >> m >> t >> sx >> sy >> fx >> fy;
+    for(int i = 0 ; i < t ; i++) {
+        int x, y;
+        cin >> x >> y;
+        vis[x][y] = -1;
+    }
+    dfs(sx, sy);
+    cout << ans << endl;
+    return 0;
+}
