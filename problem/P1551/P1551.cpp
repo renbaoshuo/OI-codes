f3076a323fb7b9a0c5b2afaa0b617f9e86b207c0
Ren Baoshuo <i@baoshuo.ren>
2020-12-03 18:27:02 +0800

    P1551 亲戚
    R43109248

---
+++ problem/P1551/P1551.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, m, p, x, y, j, f[5005];
+
+int find(int x) {
+    if (f[x] == x) return f[x];
+    return f[x] = find(f[x]);
+}
+
+int main() {
+    cin >> n >> m >> p;
+    for (int i = 1; i <= n; i++) {
+        f[i] = i;
+    }
+    for (int i = 1; i <= m; i++) {
+        cin >> x >> y;
+        f[find(x)] = find(y);
+    }
+    for (int i = 1; i <= p; i++) {
+        cin >> x >> y;
+        if (find(x) == find(y)) {
+            cout << "Yes" << endl;
+        }
+        else {
+            cout << "No" << endl;
+        }
+    }
+    return 0;
+}
