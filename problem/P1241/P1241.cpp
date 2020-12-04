b37c8f2460b97b9a4dce76758dd3d41b2a4ec70e
Ren Baoshuo <i@baoshuo.ren>
2020-12-04 08:57:24 +0800

    P1241 括号序列
    R43144924

---
+++ problem/P1241/P1241.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+char b[105];
+int n;
+string s;
+stack<int> st;
+
+int main() {
+    cin >> s;
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == '(') {
+            st.push(i);
+            b[i] = ')';
+        }
+        if (s[i] == '[') {
+            st.push(i);
+            b[i] = ']';
+        }
+        if (s[i] == ')' || s[i] == ']') {
+            if (st.empty() || b[st.top()] != s[i]) {
+                if (s[i] == ')') {
+                    b[i] = '(';
+                }
+                else {
+                    b[i] = '[';
+                }
+            }
+            else {
+                b[st.top()] = ' ';
+                st.pop();
+            }
+        }
+    }
+    for (int i = 0; i < s.size(); i++) {
+        if (b[i] == '(' || b[i] == '[') {
+            cout << b[i];
+        }
+        cout << s[i];
+        if (b[i] == ')' || b[i] == ']') {
+            cout << b[i];
+        }
+    }
+    cout << endl;
+    return 0;
+}
