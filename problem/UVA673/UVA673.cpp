c08644011409abac9bb159ec7db23e898197d4db
Ren Baoshuo <i@baoshuo.ren>
2020-12-26 19:05:26 +0800

    UVA673 平衡的括号 Parentheses Balance
    R44264935

---
+++ problem/UVA673/UVA673.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    getchar();
+    while (t--) {
+        string s;
+        stack<char> st;
+        getline(cin, s);
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(' || s[i] == '[') {
+                st.push(s[i]);
+            }
+            else if (st.empty() || s[i] == ')' && st.top() != '(' || s[i] == ']' && st.top() != '[') {
+                cout << "No" << endl;
+                goto end;
+            }
+            else {
+                st.pop();
+            }
+        }
+        if (st.empty()) {
+            cout << "Yes" << endl;
+        }
+        else {
+            cout << "No" << endl;
+        }
+    end:;
+    }
+    return 0;
+}
