b3c5de0ceaa1499c31516c68ccc8e9fc109fae59
Ren Baoshuo <i@baoshuo.ren>
2020-12-30 18:39:46 +0800

    P5682 [CSP-J2019 江西] 次大值
    R44430393

---
+++ problem/P5682/P5682.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, t, a[200005];
+
+int main() {
+    set<int> s;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> t;
+        s.insert(t);
+    }
+    if (s.size() <= 1) {
+        cout << -1 << endl;
+    }
+    else {
+        t = 0;
+        for (int i : s) {
+            a[++t] = i;
+        }
+        cout << max(a[t - 2], a[t] % a[t - 1]) << endl;
+    }
+    return 0;
+}
