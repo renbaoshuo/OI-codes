7db766b4e76e14416728451abee98e89c5bae79b
Ren Baoshuo <i@baoshuo.ren>
2020-12-30 19:27:59 +0800

    P1678 烦恼的高考志愿
    R44433175

---
+++ problem/P1678/P1678.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int m, n, q, ans, a[100005];
+
+int main() {
+    cin >> m >> n;
+    for (int i = 0; i < m; i++) {
+        cin >> a[i];
+    }
+    sort(a, a + m);
+    for (int i = 0; i < n; i++) {
+        cin >> q;
+        int p = lower_bound(a, a + m, q) - a;
+        if (p == m) {
+            ans += q - a[m - 1];
+        }
+        else if (p == 0) {
+            ans += a[0] - q;
+        }
+        else {
+            ans += min(abs(a[p] - q), abs(q - a[p - 1]));
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
