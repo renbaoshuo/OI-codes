b425bd8067ff3f92751185e9370a299b67724707
Ren Baoshuo <i@baoshuo.ren>
2020-12-30 19:09:01 +0800

    P1182 数列分段 Section II
    R44432005

---
+++ problem/P1182/P1182.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, m, a[100005], l, r, mid, ans;
+
+bool check(int x) {
+    int tot = 0, num = 0;
+    for (int i = 1; i <= n; i++) {
+        if (tot + a[i] <= x) {
+            tot += a[i];
+        }
+        else {
+            tot = a[i];
+            num++;
+        }
+    }
+    return num >= m;
+}
+int main() {
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+        l = max(l, a[i]);
+        r += a[i];
+    }
+    while (l <= r) {
+        mid = l + r >> 1;
+        if (check(mid)) {
+            l = mid + 1;
+        }
+        else {
+            r = mid - 1;
+        }
+    }
+    cout << l << endl;
+    return 0;
+}
