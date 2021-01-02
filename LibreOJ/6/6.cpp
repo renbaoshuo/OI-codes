1fdf33ad247b7966f991c8c960b939e1729cc4b4
Ren Baoshuo <i@baoshuo.ren>
2021-01-02 23:58:24 +0800

    #6. 猜数字 2
    https://loj.ac/s/963719

---
+++ LibreOJ/6/6.cpp
@@ -0,0 +1,31 @@
+#include "interaction.h"
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+vector<int> ans;
+
+int main() {
+    n = get_num();
+    for (int i = 0; i < n; i++) {
+        int l = 0, r = 1000000;
+
+        while (l <= r) {
+            int mid = (l + r) >> 1, feedback = guess(i, mid);
+
+            if (!feedback) {
+                ans.emplace_back(mid);
+                break;
+            } else {
+                if (feedback > 0) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            }
+        }
+    }
+    submit(ans);
+    return 0;
+}
