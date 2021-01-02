aa093eb81b485daa050a00a98e7ad938340df832
Ren Baoshuo <i@baoshuo.ren>
2021-01-02 23:47:30 +0800

    #3. 复读机
    https://loj.ac/s/963696

---
+++ LibreOJ/3/3.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    freopen("copycat.in", "r", stdin);
+    freopen("copycat.out", "w", stdout);
+    int t;
+    string s;
+    cin >> t;
+    while (t--) {
+        cin >> s;
+        cout << s << endl;
+    }
+    return 0;
+}
