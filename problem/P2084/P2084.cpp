ec292fa03d3599cb02574cd5a2a217eb5d2cc594
Ren Baoshuo <i@baoshuo.ren>
2021-01-02 15:00:59 +0800

    P2084 进制转换
    R44524974

---
+++ problem/P2084/P2084.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int m;
+string s;
+
+int main() {
+    cin >> m >> s;
+    for (int i = 0; i < s.size(); i++) {
+        if(s[i] == '0') {
+            continue;
+        }
+        if (i != 0) {
+            cout << '+';
+        }
+        cout << s[i] << '*' << m << '^' << s.size() - i - 1;
+    }
+    cout << endl;
+    return 0;
+}
