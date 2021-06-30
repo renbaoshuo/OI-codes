7cf2144183ac8eb300954ac6c3b7c708868ebc41
Ren Baoshuo <i@baoshuo.ren>
2021-06-30 19:33:34 +0800

    843. n皇后问题
    https://www.acwing.com/problem/content/submission/code_detail/6188239/

---
+++ AcWing/843/843.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+bool col[20], dg[20], udg[20];
+char g[20][20];
+
+void dfs(int u) {
+    if (u == n) {
+        for (int i = 0; i < n; i++) {
+            cout << g[i] << endl;
+        }
+        cout << endl;
+        return;
+    }
+    for (int i = 0; i < n; i++) {
+        if (!col[i] && !dg[u + i] && !udg[n - u + i]) {
+            g[u][i] = 'Q';
+            col[i] = dg[u + i] = udg[n - u + i] = true;
+            dfs(u + 1);
+            col[i] = dg[u + i] = udg[n - u + i] = false;
+            g[u][i] = '.';
+        }
+    }
+}
+
+int main() {
+    cin >> n;
+    for(int i = 0 ; i < n ; i++) {
+        for(int j = 0 ; j < n ; j++) {
+            g[i][j] = '.';
+        }
+    }
+    dfs(0);
+    return 0;
+}
