9e5aa29038c092ccfd8cc38f4dcce083650157c3
Ren Baoshuo <i@baoshuo.ren>
2021-07-03 15:55:42 +0800

    P2404 自然数的拆分问题
    R52333755

---
+++ Luogu/problem/P2404/P2404.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, a[10];
+
+void dfs(int x, int depth, int last) {
+    if (x > n) return;
+    if (x == n) {
+        for(int i = 1 ; i < depth - 1 ; i++) {
+            cout << a[i] << '+';
+        }
+        cout << a[depth-1] << endl;
+        return;
+    }
+    for (int i = last; i < n; i++) {
+        a[depth] = i;
+        dfs(x + i, depth + 1, i);
+        a[depth] = 0;
+    }
+    return;
+}
+
+int main() {
+    cin >> n;
+    dfs(0, 1, 1);
+    return 0;
+}
