db48c57f163e5177be3b1c002d7c5c6e5b814a6b
Ren Baoshuo <i@baoshuo.ren>
2021-07-04 19:29:50 +0800

    B2004 对齐输出
    R52393209

---
+++ Luogu/problem/B2004/B2004.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << right << setw(8) << a << ' ' << right << setw(8) << b << ' ' << right << setw(8) << c << endl;
+    return 0;
+}
