ce07771bf7ed3c659084d0703483182a70d447fa
Ren Baoshuo <i@baoshuo.ren>
2021-07-04 20:18:19 +0800

    B2005 字符三角形
    R52395350

---
+++ Luogu/problem/B2005/B2005.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    char c;
+    cin >> c;
+    cout << "  " << c << endl
+         << " " << c << c << c << endl
+         << c << c << c << c << c << endl;
+    return 0;
+}
