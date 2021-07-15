fc6f52a784e86bdc7e168d987ce65e0249bbaacd
Ren Baoshuo <i@baoshuo.ren>
2021-07-15 19:05:27 +0800

    B2012 甲流疫情死亡率
    R53229760

---
+++ Luogu/problem/B2012/B2012.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    double a, b;
+    cin >> a >> b;
+    cout << fixed << setprecision(3) << b / a * 100 << '%' << endl;
+    return 0;
+}
