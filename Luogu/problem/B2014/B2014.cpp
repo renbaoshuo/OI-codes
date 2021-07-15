66eb0168ba6272cab18f1dd36b608337f8592f93
Ren Baoshuo <i@baoshuo.ren>
2021-07-15 19:12:52 +0800

    B2014 与圆相关的计算
    R53230420

---
+++ Luogu/problem/B2014/B2014.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const double pi = 3.14159;
+
+int main() {
+    double r;
+    cin >> r;
+    cout << fixed << setprecision(4) << 2 * r << ' '
+         << fixed << setprecision(4) << 2 * pi * r << ' '
+         << fixed << setprecision(4) << pi * r * r << endl;
+    return 0;
+}
