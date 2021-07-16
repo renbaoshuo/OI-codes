3bf492026826ea1f81217bef070a5063eea69671
Ren Baoshuo <i@baoshuo.ren>
2021-07-16 10:57:48 +0800

    B2015 计算并联电阻的阻值
    R53273658

---
+++ Luogu/problem/B2015/B2015.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    double r1, r2;
+    cin >> r1 >> r2;
+    cout << fixed << setprecision(2) << 1 / (1 / r1 + 1 / r2) << endl;
+    return 0;
+}
