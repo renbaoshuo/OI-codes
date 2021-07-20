e4e1bb0e305660b63ea582b981eb76772207eb1d
Ren Baoshuo <i@baoshuo.ren>
2021-07-21 00:12:05 +0800

    P1744 采购特价商品
    R53741039

--- Luogu/problem/P1744/P1744.cpp
+++ Luogu/problem/P1744/P1744.cpp
@@ -3,11 +3,34 @@
 using namespace std;
 
 int n, m, x, y, s, t;
-double g[105][105];
 vector<pair<int, int>> d(1);
+vector<pair<int, double>> g[105];
+double dist[105];
+bool st[105];
 
 double dis(pair<int, int> a, pair<int, int> b) {
     return sqrt(pow(a.first - b.first, 2) + pow(a.second - b.second, 2));
+}
+
+void dijkstra() {
+    for (int i = 1; i <= n; i++) {
+        dist[i] = 1e9;
+    }
+    dist[s] = 0.00;
+    priority_queue<pair<double, int>, vector<pair<double, int>>, greater<pair<double, int>>> q;
+    q.push(make_pair(0.00, s));
+    while (!q.empty()) {
+        auto t = q.top();
+        q.pop();
+        if (st[t.second]) continue;
+        for (auto i : g[t.second]) {
+            if (dist[i.first] > t.first + i.second) {
+                dist[i.first] = t.first + i.second;
+                q.push(make_pair(dist[i.first], i.first));
+            }
+        }
+        st[t.second] = true;
+    }
 }
 
 int main() {
@@ -16,24 +39,14 @@ int main() {
         cin >> x >> y;
         d.push_back(make_pair(x, y));
     }
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
-            g[i][j] = i == j ? 0 : 1e9;
-        }
-    }
     cin >> m;
     for (int i = 1; i <= m; i++) {
         cin >> x >> y;
-        g[x][y] = g[y][x] = dis(d[x], d[y]);
-    }
-    for (int k = 1; k <= n; k++) {
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                g[i][j] = min(g[i][j], g[i][k] + g[k][j]);
-            }
-        }
+        g[x].push_back(make_pair(y, dis(d[x], d[y])));
+        g[y].push_back(make_pair(x, dis(d[x], d[y])));
     }
     cin >> s >> t;
-    cout << fixed << setprecision(2) << g[s][t] << endl;
+    dijkstra();
+    cout << fixed << setprecision(2) << dist[t] << endl;
     return 0;
 }
