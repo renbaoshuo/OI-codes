20d4ca5d3117ae7aec1089aeb167b8a268cdbf69
Ren Baoshuo <i@baoshuo.ren>
2021-07-23 19:54:33 +0800

    B2051 点和正方形的关系
    R54020455

---
+++ Luogu/problem/B2051/B2051.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int x, y;
+
+int main() {
+    cin >> x >> y;
+    cout << (-1 <= x && x <= 1 && -1 <= y && y <= 1 ? "yes" : "no") << endl;
+    return 0;
+}
