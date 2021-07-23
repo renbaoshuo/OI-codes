68a6439641a7d849a5409162db074caa88ceb4dc
Ren Baoshuo <i@baoshuo.ren>
2021-07-23 08:51:54 +0800

    CF1553A Digits Sum
    R53955241

---
+++ Luogu/problem/CF1553A/CF1553A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int t, n;
+
+int main() {
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        cout << (n + 1) / 10 << endl;
+    }
+    return 0;
+}
