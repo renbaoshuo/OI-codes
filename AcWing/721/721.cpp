34d225bd06cbe1c29be0de044693757bfb04fcc1
Ren Baoshuo <i@baoshuo.ren>
2021-08-01 19:35:56 +0800

    721. 递增序列
    https://www.acwing.com/problem/content/submission/code_detail/6826700/

---
+++ AcWing/721/721.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+int main() {
+    while (cin >> n, n) {
+        for (int i = 1; i <= n; i++) {
+            cout << i << ' ';
+        }
+        cout << endl;
+    }
+    return 0;
+}
