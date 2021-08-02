ceb66a4db878bc61be8f74a82748f6dc8214a279
Ren Baoshuo <i@baoshuo.ren>
2021-08-02 19:36:42 +0800

    608. 差
    https://www.acwing.com/problem/content/submission/code_detail/6847391/

---
+++ AcWing/608/608.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << "DIFERENCA = " << (a * b - c * d) << endl;
+    return 0;
+}
