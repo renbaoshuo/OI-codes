33bdf7d7489e5ab627d52449b61cddb4d1e97da4
Ren Baoshuo <i@baoshuo.ren>
2021-08-02 11:01:39 +0800

    654. 时间转换
    https://www.acwing.com/problem/content/submission/code_detail/6835409/

---
+++ AcWing/654/654.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+    cout << a / 3600 << ':' << a % 3600 / 60 << ':' << a % 60 << endl;
+    return 0;
+}
