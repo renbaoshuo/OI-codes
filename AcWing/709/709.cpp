a7cc8496ae9ad310ab3a98563d69a8b890c2d671
Ren Baoshuo <i@baoshuo.ren>
2021-08-03 14:43:04 +0800

    709. 奇数
    https://www.acwing.com/problem/content/submission/code_detail/6862921/

---
+++ AcWing/709/709.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+    for (int i = 1; i <= x; i++) {
+        if (i & 1) cout << i << endl;
+    }
+    return 0;
+}
