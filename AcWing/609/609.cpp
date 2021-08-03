702561f5fbb69df405f9373f6c48ff8a8270ee7d
Ren Baoshuo <i@baoshuo.ren>
2021-08-03 14:51:25 +0800

    609. 工资
    https://www.acwing.com/problem/content/submission/code_detail/6863153/

---
+++ AcWing/609/609.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    double c;
+    cin >> a >> b >> c;
+    cout << "NUMBER = " << a << endl;
+    cout << "SALARY = U$ " << fixed << setprecision(2) << b * c << endl;
+    return 0;
+}
