7f1abb072695b7b5b92bf5da2ca5d05dc89f6d6c
Ren Baoshuo <i@baoshuo.ren>
2021-08-06 18:28:57 +0800

    772. 只出现一次的字符
    https://www.acwing.com/problem/content/submission/code_detail/6942793/

--- AcWing/772/772.cpp
+++ AcWing/772/772.cpp
@@ -4,30 +4,20 @@ using namespace std;
 
 int main() {
     string s;
-    vector<char> ans;
-    int a = -1;
-    char c = 'a';
+    map<char, int> m;
+    queue<char> q;
     cin >> s;
-    map<char, int> m, m1;
-    for (int i = 0; i < s.size(); i++) {
-        m[s[i]]++;
-        if (!m1.count(s[i])) m1[s[i]] = i;
+    for (char i : s) {
+        m[i]++;
+        q.push(i);
     }
-    for (char i = 'a'; i <= 'z'; i++) {
-        if (m[i] == 1) {
-            ans.push_back(i);
+    while (!q.empty()) {
+        if (m[q.front()] == 1) {
+            cout << q.front() << endl;
+            exit(0);
         }
+        q.pop();
     }
-    if (ans.empty()) {
-        cout << "no" << endl;
-    } else {
-        for (char i : ans) {
-            if (a == -1 || m1[i] < a) {
-                a = m1[i];
-                c = i;
-            }
-        }
-        cout << c << endl;
-    }
+    cout << "no" << endl;
     return 0;
 }
