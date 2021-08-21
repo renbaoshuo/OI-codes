f593a4176c98a4d0bf597deb16bbba6f81a3d3cf
Ren Baoshuo <i@baoshuo.ren>
2021-08-21 18:52:53 +0800

    #636. 0819购物 (shopping)
    https://sjzezoj.com/submission/19204

---
+++ S2OJ/636/636.cpp
@@ -0,0 +1,50 @@
+#pragma GCC optimize(2)
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct node {
+    long long p, q;
+} a[50005];
+
+int n, k;
+long long m, ans;
+priority_queue<long long, vector<long long>, greater<long long>> q;
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    cin >> n >> k >> m;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i].p >> a[i].q;
+    }
+    sort(a, a + n, [](node a, node b) { return a.q < b.q; });
+    for (int i = 0; i < k; i++) {
+        ans += a[i].q;
+        q.push(a[i].p - a[i].q);
+        if (ans > m) {
+            cout << i << endl;
+            exit(0);
+        }
+        if (i == n - 1) {
+            cout << n << endl;
+            exit(0);
+        }
+    }
+    sort(a + k, a + n, [](node a, node b) { return a.p < b.p; });
+    for (int i = k; i < n; i++) {
+        auto t = q.top();
+        ans += a[i].p;
+        if (a[i].q + t < a[i].p) {
+            q.pop();
+            ans += t + a[i].q - a[i].p;
+            q.push(a[i].p - a[i].q);
+        }
+        if (ans > m) {
+            cout << i << endl;
+            exit(0);
+        }
+    }
+    cout << n << endl;
+    return 0;
+}
