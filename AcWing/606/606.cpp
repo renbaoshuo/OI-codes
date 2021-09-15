6937a91b97be3afc00af2126f25846033a05f69c
Ren Baoshuo <i@baoshuo.ren>
2021-09-15 20:01:37 +0800

    606. 平均数1
    https://www.acwing.com/problem/content/submission/code_detail/7742440/

---
+++ AcWing/606/606.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+double a, b;
+
+int main() {
+    cin >> a >> b;
+    cout << "MEDIA = " << fixed << setprecision(5) << (a * 3.5 + b * 7.5) / 11 << endl;
+    return 0;
+}
