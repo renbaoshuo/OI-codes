c10aad441a768b1b5b14a5f5e83e9d37194ce836
Ren Baoshuo <i@baoshuo.ren>
2021-09-15 19:46:31 +0800

    604. 圆的面积
    https://www.acwing.com/problem/content/submission/code_detail/7741979/

---
+++ AcWing/604/604.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const double pi = 3.14159;
+
+int main() {
+    double r;
+    cin >> r;
+    cout << "A=" << fixed << setprecision(4) << pi * r * r << endl;
+    return 0;
+}
