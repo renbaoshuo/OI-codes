69089b2dc5866af13587893fc38d19841f5171b3
Ren Baoshuo <i@baoshuo.ren>
2021-09-21 21:08:56 +0800

    1129. 热浪
    https://www.acwing.com/problem/content/submission/code_detail/7845524/

---
+++ AcWing/1129/1129.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, m, s, t, u, v, w, dist[2505];
+vector<pair<int, int>> g[2505];
+bool vis[2505];
+
+int main() {
+    cin >> n >> m >> s >> t;
+    for (int i = 0; i < m; i++) {
+        cin >> u >> v >> w;
+        g[u].push_back(make_pair(v, w));
+        g[v].push_back(make_pair(u, w));
+    }
+    memset(dist, 0x3f, sizeof(dist));
+    dist[s] = 0;
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
+    q.push(make_pair(0, s));
+    while (!q.empty()) {
+        auto t = q.top();
+        q.pop();
+        if (vis[t.second]) continue;
+        for (auto i : g[t.second]) {
+            if (dist[i.first] > t.first + i.second) {
+                dist[i.first] = t.first + i.second;
+                q.push(make_pair(dist[i.first], i.first));
+            }
+        }
+        vis[t.second] = true;
+    }
+    cout << dist[t] << endl;
+    return 0;
+}
