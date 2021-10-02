378abf1efc49175860e5dc8d0e2f2280c935dee8
Ren Baoshuo <i@baoshuo.ren>
2021-10-02 16:55:20 +0800

    P6. 文件 IO 测试
    https://hydro.ac/d/system_test/record/61581e4f9f554addb798a8a0

---
+++ Hydro/system_test/6/6.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    freopen("a+b.in", "r", stdin);
+    freopen("a+b.out", "w", stdout);
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << endl;
+    return 0;
+}
