3dedd00a704405a75c92762c913a8f2df52f3fa6
Ren Baoshuo <i@baoshuo.ren>
2021-10-24 15:44:24 +0800

    P7909 [CSP-J 2021] 分糖果（民间数据）
    R60830182

---
+++ Luogu/problem/P7909/P7909.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, l, r, ans;
+
+int main() {
+    cin >> n >> l >> r;
+    cout << min((int)(ceil(1.0 * l / n) * n + n - 1), r) % n << endl;
+    return 0;
+}
