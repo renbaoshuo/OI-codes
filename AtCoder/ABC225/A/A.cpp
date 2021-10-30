2250e7703487e45adccfea532114efb4f3054605
Ren Baoshuo <i@baoshuo.ren>
2021-10-30 20:07:24 +0800

    A - Distinct Strings
    https://atcoder.jp/contests/abc225/submissions/26895315

---
+++ AtCoder/ABC225/A/A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int ans;
+string s;
+
+int main() {
+    cin >> s;
+    sort(s.begin(), s.end());
+    do ans++;
+    while (next_permutation(s.begin(), s.end()));
+    cout << ans << endl;
+    return 0;
+}
