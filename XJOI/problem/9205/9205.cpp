4b68402aafc293d15d6d92b58edbcf88400f122f
Ren Baoshuo <i@baoshuo.ren>
2021-11-11 10:40:29 +0800

    9205. 长方体表面积与体积
    https://xjoi.net/detail/1162333

---
+++ XJOI/problem/9205/9205.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << 2 * (a * b + b * c + a * c) << ' ' << a * b * c << endl;
+    return 0;
+}
