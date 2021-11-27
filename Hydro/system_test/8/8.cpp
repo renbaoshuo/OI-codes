e4fca64680d6afd6473305af4de57e86fd50dbc2
Ren Baoshuo <i@baoshuo.ren>
2021-11-27 09:15:57 +0800

    P8. Special Judge 测试
    https://hydro.ac/d/system_test/record/61a1868253a50c11fd2bd7b8

---
+++ Hydro/system_test/8/8.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int a, b;
+
+int main() {
+    cin >> a >> b;
+    cout << a + b << endl;
+    return 0;
+}
---
+++ Hydro/system_test/8/data/checker.cc
@@ -0,0 +1,11 @@
+#include "testlib.h"
+
+int main(int argc, char* argv[]) {
+    setName("compares two signed integers");
+    registerTestlibCmd(argc, argv);
+    int ja = ans.readInt();
+    int pa = ouf.readInt();
+    if (ja != pa)
+        quitf(_wa, "expected %d, found %d", ja, pa);
+    quitf(_ok, "answer is %d", ja);
+}
