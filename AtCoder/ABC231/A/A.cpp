0678e7c2fdd4ad8076e89e28badffd19cf221f19
Baoshuo <i@baoshuo.ren>
2021-12-11 20:02:56 +0800

    A - Water Pressure
    https://atcoder.jp/contests/abc231/submissions/27817073

---
+++ AtCoder/ABC231/A/A.cpp
@@ -0,0 +1,14 @@
+#include <iomanip>
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int d;
+
+int main() {
+    cin >> d;
+    cout << double(d / 100.0) << endl;
+    return 0;
+}
