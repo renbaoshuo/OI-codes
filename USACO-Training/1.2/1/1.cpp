1a72738cb61308bcb6eeb2fa1231734b609b28d6
Baoshuo <i@baoshuo.ren>
2021-12-17 21:35:51 +0800

    Submitting Solutions

---
+++ USACO-Training/1.2/1/1.cpp
@@ -0,0 +1,20 @@
+/*
+ID: baoshuo1
+TASK: test
+LANG: C++
+*/
+
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+    freopen("test.in", "r", stdin);
+    freopen("test.out", "w", stdout);
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << endl;
+    return 0;
+}
