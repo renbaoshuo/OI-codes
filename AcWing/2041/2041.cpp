31edb8e60169670a2744756ba3c7e3c66231cb41
Baoshuo <i@baoshuo.ren>
2022-01-03 18:55:28 +0800

    2041. 干草堆
    https://www.acwing.com/problem/content/submission/code_detail/9659094/

---
+++ AcWing/2041/2041.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+const int N = 1000005;
+
+int n, k, a, b, s[N], f[N];
+
+int main() {
+    cin >> n >> k;
+    for (int i = 1; i <= k; i++) {
+        cin >> a >> b;
+        s[a] += 1;
+        s[b + 1] -= 1;
+    }
+    for (int i = 1; i <= n; i++) {
+        f[i] = f[i - 1] + s[i];
+    }
+    std::sort(f + 1, f + 1 + n);
+    cout << f[n / 2 + 1] << endl;
+    return 0;
+}
