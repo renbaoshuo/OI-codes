8ef4b3dbe887e147716b0cf76dc73737ef476efb
Baoshuo Ren <i@baoshuo.ren>
2022-03-05 20:05:58 +0800

    A - T-shirt
    https://atcoder.jp/contests/abc242/submissions/29868633

---
+++ AtCoder/ABC242/A/A.cpp
@@ -0,0 +1,21 @@
+#include <iomanip>
+#include <iostream>
+
+using std::cin;
+using std::cout;
+const char endl = '\n';
+
+int a, b, c, x;
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    cin >> a >> b >> c >> x;
+    if (x <= a) {
+        cout << 1 << endl;
+    } else if (x > b) {
+        cout << 0 << endl;
+    } else {
+        cout << std::fixed << std::setprecision(10) << 1.00 * c / (b - a) << endl;
+    }
+    return 0;
+}
