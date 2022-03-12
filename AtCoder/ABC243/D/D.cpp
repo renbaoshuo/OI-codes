48722a94773d1dde89274880f2ad17d63e2a34a6
Baoshuo Ren <i@baoshuo.ren>
2022-03-12 21:02:25 +0800

    D - Moves on Binary Tree
    https://atcoder.jp/contests/abc243/submissions/30064173

---
+++ AtCoder/ABC243/D/D.cpp
@@ -0,0 +1,41 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::cin;
+using std::cout;
+const char endl = '\n';
+
+std::vector<int> a;
+
+int n, t;
+long long x;
+std::string s;
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    cin >> n >> x >> s;
+    std::reverse(s.begin(), s.end());
+    for (char &c : s) {
+        if (c == 'U') {
+            t++;
+            c = ' ';
+        } else if (t) {
+            t--;
+            c = ' ';
+        }
+    }
+    while (t--) x >>= 1;
+    std::reverse(s.begin(), s.end());
+    for (char c : s) {
+        if (c == 'L') {
+            x <<= 1;
+        } else if (c == 'R') {
+            x <<= 1;
+            x |= 1;
+        }
+    }
+    cout << x << endl;
+    return 0;
+}
