6ed827d570aaef4e249466346314f1f2b0521e9b
Baoshuo <i@baoshuo.ren>
2022-03-17 19:45:08 +0800

    B2024 输出浮点数
    R71616151

---
+++ Luogu/B2024/B2024.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+
+double x;
+
+int main() {
+    scanf("%lf", &x);
+    printf(
+        "%f\n"
+        "%.5f\n"
+        "%e\n"
+        "%g\n",
+        x, x, x, x);
+    return 0;
+}
