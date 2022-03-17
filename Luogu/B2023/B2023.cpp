7ddf30bee663dee88dba313ff2ae3be090cb53c3
Baoshuo <i@baoshuo.ren>
2022-03-17 19:43:06 +0800

    B2023 空格分隔输出
    R71615916

---
+++ Luogu/B2023/B2023.cpp
@@ -0,0 +1,18 @@
+#include <iomanip>
+#include <iostream>
+
+using std::cin;
+using std::cout;
+const char endl = '\n';
+
+char a;
+int b;
+float c;
+double d;
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    cin >> a >> b >> c >> d;
+    cout << a << ' ' << b << ' ' << std::fixed << std::setprecision(6) << c << ' ' << std::fixed << std::setprecision(6) << d << endl;
+    return 0;
+}
