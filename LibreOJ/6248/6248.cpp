ab566c92ab6e31ff612dd8ffe371d38f16327296
Baoshuo <i@baoshuo.ren>
2022-03-23 14:19:09 +0800

    #6248. 「CodePlus 2017 11 月赛」晨跑
    https://loj.ac/s/1420425

---
+++ LibreOJ/6248/6248.cpp
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <iostream>
+
+using std::cin;
+using std::cout;
+const char endl = '\n';
+
+long long a, b, c;
+
+inline long long lcm(long long a, long long b) {
+    return a / std::__gcd(a, b) * b;
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+
+    cin >> a >> b >> c;
+    cout << lcm(a, lcm(b, c)) << endl;
+
+    return 0;
+}
