970efada900fcbfb8b2620b90a23604564f7a529
Baoshuo <i@baoshuo.ren>
2022-03-26 20:08:54 +0800

    B - Mex
    https://atcoder.jp/contests/abc245/submissions/30437957

---
+++ AtCoder/ABC245/B/B.cpp
@@ -0,0 +1,27 @@
+#include <algorithm>
+#include <iostream>
+
+using std::cin;
+using std::cout;
+const char endl = '\n';
+
+const int N = 2005;
+
+int n;
+bool vis[N];
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    cin >> n;
+    for (int i = 1, x; i <= n; i++) {
+        cin >> x;
+        vis[x] = true;
+    }
+    for (int i = 0; i <= 2000; i++) {
+        if (!vis[i]) {
+            cout << i << endl;
+            exit(0);
+        }
+    }
+    return 0;
+}
