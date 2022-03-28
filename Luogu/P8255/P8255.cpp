e4a539a85b6ae470632acd2940863b230c5b2ae1
Baoshuo <i@baoshuo.ren>
2022-03-28 21:53:09 +0800

    [NOI Online 2022 普及组] 数学游戏（民间数据）
    R72648237

---
+++ Luogu/P8255/P8255.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+
+using std::cin;
+using std::cout;
+const char endl = '\n';
+
+int t;
+long long x, z;
+
+int main() {
+    std::ios::sync_with_stdio(false);
+
+    cin >> t;
+    while (t--) {
+        cin >> x >> z;
+
+        if (!z) {
+            cout << 0 << endl;
+            continue;
+        }
+
+        long long yg = z / x,
+                  gg = std::__gcd(x * x, yg),
+                  g = std::sqrt(gg);
+
+        if (gg != g * g || z % x) {
+            cout << -1 << endl;
+        } else {
+            cout << yg / g << endl;
+        }
+    }
+
+    return 0;
+}
