e192ddb501f1443bab745531e426be5119d60c4f
Baoshuo <i@baoshuo.ren>
2022-03-31 22:46:31 +0800

    B - Vlad and Candies
    https://codeforces.com/contest/1660/submission/151521823

---
+++ CodeForces/1660/B/B.cpp
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <iostream>
+
+using std::cin;
+using std::cout;
+const char endl = '\n';
+
+const int N = 2e5 + 5;
+
+int t, n, a[N];
+
+int main() {
+    std::ios::sync_with_stdio(false);
+
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        for (int i = 1; i <= n; i++) {
+            cin >> a[i];
+        }
+        std::sort(a + 1, a + n + 1);
+        if (n == 1) {
+            cout << (a[1] == 1 ? "YES" : "NO") << endl;
+        } else {
+            cout << (a[n] - a[n - 1] > 1 ? "NO" : "YES") << endl;
+        }
+    }
+
+    return 0;
+}
