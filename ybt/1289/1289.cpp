a4a780f56e291b934d2e9c5ad6cb0dfde4d2a94b
Baoshuo <i@baoshuo.ren>
2022-04-03 10:40:41 +0800

    1289. 拦截导弹
    http://ybt.ssoier.cn:8088/statusx.php?runidx=15580224

---
+++ ybt/1289/1289.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+const char endl = '\n';
+
+const int N = 20005;
+
+int n, a[N], f[N], ans;
+
+int main() {
+    std::ios::sync_with_stdio(false);
+
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+    }
+
+    for (int i = 1; i <= n; i++) {
+        f[i] = 1;
+        for (int j = 1; j < i; j++) {
+            if (a[j] >= a[i]) f[i] = std::max(f[i], f[j] + 1);
+        }
+        ans = std::max(ans, f[i]);
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
