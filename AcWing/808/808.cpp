d89ee4b62de5e3ce0be62d9fc42788c4cd93db5c
Baoshuo <i@baoshuo.ren>
2022-05-09 19:12:05 +0800

    808. 最大公约数
    https://www.acwing.com/problem/content/submission/code_detail/14063749/

---
+++ AcWing/808/808.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <iostream>
+
+using std::cin;
+using std::cout;
+const char endl = '\n';
+
+int a, b;
+
+int main() {
+    std::ios::sync_with_stdio(false);
+
+    cin >> a >> b;
+    cout << std::__gcd(a, b) << endl;
+
+    return 0;
+}
