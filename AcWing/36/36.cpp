e0b77729a1508c812bccfd879f8045cb8a195331
Baoshuo <i@baoshuo.ren>
2022-05-09 19:33:30 +0800

    36. 合并两个排序的链表
    https://www.acwing.com/problem/content/submission/code_detail/14064359/

---
+++ AcWing/36/36.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+  public:
+    ListNode* merge(ListNode* l1, ListNode* l2) {
+        if (l1 == nullptr) {
+            return l2;
+        } else if (l2 == nullptr) {
+            return l1;
+        } else if (l1->val < l2->val) {
+            l1->next = merge(l1->next, l2);
+            return l1;
+        } else {
+            l2->next = merge(l1, l2->next);
+            return l2;
+        }
+    }
+};
