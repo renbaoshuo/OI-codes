6c0df66cbc7ce1b16ed89d2d6837b2772a90baad
Baoshuo <i@baoshuo.ren>
2022-05-14 17:52:50 +0800

    A - AvtoBus
    https://codeforces.com/contest/1679/submission/157150402

---
+++ Codeforces/1679/A/A.cpp
@@ -0,0 +1,27 @@
+#include <algorithm>
+#include <iostream>
+
+using std::cin;
+using std::cout;
+const char endl = '\n';
+
+int t;
+long long a;
+
+int main() {
+    std::ios::sync_with_stdio(false);
+
+    cin >> t;
+
+    while (t--) {
+        cin >> a;
+
+        if (a % 2 == 0 && a >= 4) {
+            cout << a / 6 + !!(a % 6) << ' ' << (a % 4 == 2 ? (a - 3) / 4 + 1 : a / 4) << endl;
+        } else {
+            cout << -1 << endl;
+        }
+    }
+
+    return 0;
+}
