07f2d4da0aac147bf51487f8340cca560f230c41
Baoshuo <i@baoshuo.ren>
2022-06-06 16:37:49 +0800

    #1. A + B Problem
    https://uoj.ac/submission/557214

---
+++ UniversalOJ/1/1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+const char endl = '\n';
+
+int a, b;
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> a >> b;
+    cout << a + b << endl;
+
+    return 0;
+}
