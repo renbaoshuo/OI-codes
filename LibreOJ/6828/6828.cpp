4d158adea962d6b57406804797dba2b0885f3022
Baoshuo <i@baoshuo.ren>
2022-06-27 11:31:52 +0800

    #6828. 「THUPC 2022」想象
    https://loj.ac/s/1492413

---
+++ LibreOJ/6828/6828.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+const char endl = '\n';
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cout << "FJOI" << endl;
+
+    return 0;
+}
