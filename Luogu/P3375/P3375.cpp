17e280db2ac8c922c3e88fb0dfe5bc5f99a62962
Baoshuo <i@baoshuo.ren>
2022-10-28 17:27:23 +0800

    P3375 【模板】KMP字符串匹配
    https://www.luogu.com.cn/record/91905614

--- Luogu/P3375/P3375.cpp
+++ Luogu/P3375/P3375.cpp
@@ -3,32 +3,41 @@
 
 using std::cin;
 using std::cout;
-using std::endl;
+const char endl = '\n';
 
-const int N = 1000005;
+const int N = 1e6 + 5;
 
-int f[N], next[N];
+int next[N], f[N];
 std::string s1, s2;
 
 int main() {
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     cin >> s1 >> s2;
+
     next[0] = -1;
+
     for (int i = 1, j = -1; i < s2.size(); i++) {
-        while (j != -1 && s2[i] != s2[j + 1]) j = next[j];
+        while (~j && s2[i] != s2[j + 1]) j = next[j];
         if (s2[i] == s2[j + 1]) j++;
         next[i] = j;
     }
+
     for (int i = 0, j = -1; i < s1.size(); i++) {
-        while (j != -1 && (j == s2.size() || s1[i] != s2[j + 1])) j = next[j];
+        while (~j && (j == s2.size() || s1[i] != s2[j + 1])) j = next[j];
         if (s1[i] == s2[j + 1]) j++;
-        if (j == s2.size() - 1) {
+        if (j + 1 == s2.size()) {
             cout << i - j + 1 << endl;
             f[i] = j;
         }
     }
+
     for (int i = 0; i < s2.size(); i++) {
         cout << next[i] + 1 << ' ';
     }
+
     cout << endl;
+
     return 0;
 }
