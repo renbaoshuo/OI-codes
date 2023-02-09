2600cf97f9270e90ba47843b3a53bb9ee49367d9
Baoshuo <i@baoshuo.ren>
2023-02-09 21:24:20 +0800

    #1905. 【AHOI2013】连通图
    https://sjzezoj.com/submission/68776

---
+++ S2OJ/1905/data/std.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a219db2521834cf541989205a55a091d018742de9251484e712d6836a013f706
+size 3055
---
+++ S2OJ/1905/data/val.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0f6870ff5847eb6aeabab3423f26ba486e1325243e57953efa0787aec6358eaa
+size 1137
