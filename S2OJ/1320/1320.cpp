a9cd8b60022e168cb089b7b2b3c8a65148d170cb
Baoshuo <i@baoshuo.ren>
2023-02-28 11:21:35 +0800

    #1320. 【20230228省选模拟赛T3】Rev
    https://sjzezoj.com/submission/72867

---
+++ S2OJ/1320/1320.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <utility>
+#include <vector>
+
+using std::cin;
+using std::cout;
+const char endl = '\n';
+
+std::string s, t;
+std::vector<std::pair<int, int>> ans;
+
+bool check() {
+    if (std::count(s.begin(), s.end(), '1') != std::count(t.begin(), t.end(), '1')) {
+        return false;
+    }
+
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == t[i]) continue;
+
+        int cnt = 0;
+        bool flag = false;
+
+        for (int j = i; j < s.size(); j++) {
+            if (s[j] == '1') cnt++;
+
+            if (s[j] == t[i] && cnt % 2 == 0) {
+                flag = true;
+                ans.emplace_back(i + 1, j + 1);
+                std::reverse(s.begin() + i, s.begin() + j + 1);
+                break;
+            }
+        }
+
+        if (!flag) return false;
+    }
+
+    return true;
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> s >> t;
+
+    if (check()) {
+        cout << "YES" << endl
+             << ans.size() << endl;
+
+        for (auto e : ans) {
+            cout << e.first << ' ' << e.second << endl;
+        }
+    } else {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
---
+++ S2OJ/1320/data/chk.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5e30d1300c35e03f305a796df060dbe588071ad5577aea3f81b793f5395f499f
+size 718
