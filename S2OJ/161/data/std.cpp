37cb0c04e9a073b1d8e78c628cce847b8f879c85
Baoshuo <i@baoshuo.ren>
2023-03-02 21:37:04 +0800

    #161. 【模板】最小斯坦纳树
    https://sjzezoj.com/submission/73105

---
+++ S2OJ/161/data/std.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8ecf9f098fa1fd76d468d179da0ced97171d1fd7a3bf24f75b25facfce79123a
+size 1156
---
+++ S2OJ/161/data/val.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a1e9cba5f935b8d21352fc5d187245d5f38015e66d804f3c9067f0a58b907031
+size 891
