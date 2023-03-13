44bb4f9b653d01961fa1cab6d8a9050207e0bf8b
Baoshuo <i@baoshuo.ren>
2023-03-14 06:40:22 +0800

    #1982. 【春季测试 2023】涂色游戏
    https://sjzezoj.com/submission/73273

---
+++ S2OJ/1982/1982.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+
+using std::cin;
+using std::cout;
+const char endl = '\n';
+
+void solve() {
+    int n, m, q;
+
+    cin >> n >> m >> q;
+
+    std::vector<std::pair<int, int>> g(n + 1), h(m + 1);
+
+    for (int i = 1, op, x, c; i <= q; i++) {
+        cin >> op >> x >> c;
+
+        if (op == 0) {
+            g[x] = std::make_pair(c, i);
+        } else {  // op == 1
+            h[x] = std::make_pair(c, i);
+        }
+    }
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            if (g[i].second > h[j].second) {
+                cout << g[i].first;
+            } else {
+                cout << h[j].first;
+            }
+
+            if (j < m) {
+                cout << ' ';
+            } else {  // j == m
+                cout << endl;
+            }
+        }
+    }
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t;
+
+    cin >> t;
+
+    while (t--) solve();
+
+    return 0;
+}
